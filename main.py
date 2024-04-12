5ba5c1e6b4a44833510d1f98affb41643b30cca6
Jynxziii <166734431+Jynxziii@users.noreply.github.com>
2024-04-12 12:30:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 game = GemGame()
 while True:
     print("\n=== Gem Game ===")
-    print("1. Play a Challenge (gives 10 gems)")
+    print("1. Play a Challenge (gives 5 gems)")
     print("2. Spin Wheel (costs 10 gems)")
     print("3. Show Gem Balance")
     print("4. Quit")

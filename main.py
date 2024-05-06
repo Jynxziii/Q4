b73cc6ff7d76586ef6b0c0240cd66900a4253f3b
Jynxziii <166734431+Jynxziii@users.noreply.github.com>
2024-05-06 11:43:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def display_balance():
   print("\n{|You have", gems, "gems|}")
 #challenge to make gems
 def challenge(): 
-    global gems
+    global gems 
     questions = {"\nWhat color is a frog?": "green", "\nWhat color is a flamingo?": "pink", "\nWhat color is the sky?": "blue"} 
     question, answer = random.choice(list(questions.items()))
     print(question)

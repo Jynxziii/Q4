f7ca2f3e110943887671a51fce878967962699e3
Jynxziii <166734431+Jynxziii@users.noreply.github.com>
2024-05-20 11:59:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,20 @@ print('''               _
 | (_| (_| \__ \ | | | | (_) |
  \___\__,_|___/_|_| |_|\___/ ''')
 gems = 0 
- 
-print("mr williams i worked on my project for most of the class period today plz dont take off points i will update later ")
+
+
 def display_balance():  
   print("\n{|You have", gems, "gems|}") 
 #challenge to make gems
 def challenge(): 
     global gems   
-    questions = {"\nWhat color is a frog?": "green", "\nWhat color is a flamingo?": "pink", "\nWhat color is the sky?": "blue"} 
+    questions = { "\nWhat color is a frog?": "green", 
+                 "\nWhat color is a flamingo?": "pink", 
+                 "\nWhat color is the sky?": "blue", 
+                 "\nWhat color is a bannana?": "yellow", 
+                 "\nWhat color is a strawberry?": "red",
+                 "\nWhat color is a blueberry?": "blue", 
+                 "\nWhat color is a raspberry?": "red"} 
     question, answer = random.choice(list(questions.items()))
     print(question) 
     user_answer=input("\nEnter your answer: ")
@@ -32,10 +38,29 @@ def spin_wheel():
         prize = random.choice(["Pencil", "Eraser", "Gum", "Nothing"])
         print(f"Spinning the wheel... You won: {prize}")
         display_balance()
+
+def question():
+    global gems 
+    questions = {"\nWhat is the capital of France?": "Paris",
+                "\nWho is the current President of the United States?": "Joe Biden",
+                "\nWhat is the population of Japan?": "125 million",
+                "\nWhen did World War II end?": "1945",
+                "\nWhat is the tallest mountain in the world?": "Mount Everest",
+                "\nWho wrote 'To Kill a Mockingbird'?": "Harper Lee",
+                "\nHow many degrees is the boiling point of water in Celsius?": "100",
+                "\nHow many continents are there in the world?": "7",
+                "\nWhat is the chemical symbol for gold?": "Au", } 
+    question, answer = random.choice(list(questions.items()))
+    print(question) 
+    user_answer=input("\nEnter your answer: ")
+    if user_answer == answer: 
+        print("\n Congradulations, you earned 30 diamonds!")
+        gems += 30
+    else:
+        print("\n Wrong answer! Try again.")
+
 def Pick_A_Number():
   global gems
-  if gems < 10:
-            print("\nSorry, you don't have enough gems to spin.")
   if random.random()==3:
     gems*=15
     print("you have x15 your gems!!")
@@ -57,16 +82,17 @@ def DoubleGems():
     gems = 0
     print("You lost all your gems!")
     display_balance()
-  
+
 #UI that the user sees when game starts.
 while True:
     print("\n<<=== Gem Game ===>>")
-    print("1. Play a Challenge (gives 10 gems)")
-    print("2. Spin Wheel (costs 10 gems)")
-    print("3. Show Gem Balance")
-    print("4. Double Gems (costs 20 gems)")
-    print("5. Quit")
-    print("6. Pick a number 1-10(costs 30 gems) x15 your gems!")
+    print("1. Play a Color guesing game (gives 10 gems)")
+    print("2. Answer dificult questions (gives 30 gems)")
+    print("3. Spin Wheel (costs 10 gems)")
+    print("4. Show Gem Balance")
+    print("5. Double Gems (costs 20 gems)")
+    print("6. Quit")
+    print("7. Pick a number 1-10(costs 30 gems) x15 your gems!")
     print("wheel prizes: pencil, eraser, gum, nothing \n")
     print("\n")
 
@@ -75,17 +101,15 @@ while True:
     if choice == "1":
         challenge()
     elif choice == "2":
-        spin_wheel()
+        question()
     elif choice == "3":
-        display_balance()
+        spin_wheel()
     elif choice == "4":
-        DoubleGems()
+        display_balance()
     elif choice == "5":
-        print("Thanks for playing!")
+        DoubleGems()
     elif choice == "6":
-        Pick_A_Number()
-      break
+        print("Thanks for playing!")
+        break
     else:
         print("Invalid choice. Please choose again.")
-
-
